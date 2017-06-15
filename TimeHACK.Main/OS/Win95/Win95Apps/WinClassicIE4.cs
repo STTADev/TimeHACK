fe8f8bf66c1b816291baaa268765ce1640b3c515
lempamo <lempamo@outlook.com>
2017-06-14 20:42:28 -0400

    got IE4 back to full working order

--- TimeHACK.Main/OS/Win95/Win95Apps/WinClassicIE4.cs
+++ TimeHACK.Main/OS/Win95/Win95Apps/WinClassicIE4.cs
@@ -12,6 +12,9 @@ namespace TimeHACK.OS.Win95.Win95Apps
         public List<string> browsinghistory = new List<string>();
         public int historylocation = 0;
 
+        public HtmlDocument currentsite;
+        private Timer loadplz = new Timer();
+
         public WinClassicIE4()
         {
             InitializeComponent();
@@ -24,6 +27,8 @@ namespace TimeHACK.OS.Win95.Win95Apps
             hidePrograms();
             browsinghistory.Add("www.microsoft.com/internetexplorer4/welcome");
             for (int i = 0; i < 99; i++) browsinghistory.Add(null);
+            webBrowser1.DocumentText = resources.GetString("ie4start_HTML");
+            webBrowser1.Show();
             foreach (ToolStripMenuItem item in MenuStrip3.Items) item.Font = new Font(TitleScreen.pfc.Families[0], 16F, FontStyle.Regular, GraphicsUnit.Point, ((0)));
             foreach (Control ctrl in Panel1.Controls) ctrl.Font = new Font(TitleScreen.pfc.Families[0], 16F, FontStyle.Regular, GraphicsUnit.Point, ((0)));
         }
@@ -44,6 +49,50 @@ namespace TimeHACK.OS.Win95.Win95Apps
             email3.Hide();
             hotmailpadams.Hide();
         }
+
+        private void google_LinkClicked(object sender, HtmlElementEventArgs e)
+        {
+            goToSite("www.google.com", false);
+        }
+
+        private void padams_LinkClicked(object sender, HtmlElementEventArgs e)
+        {
+            goToSite("www.12padams.com", false);
+        }
+
+        private void googleprototypelink_LinkClicked(object sender, HtmlElementEventArgs e)
+        {
+            goToSite("www.google.stanford.edu", false);
+        }
+
+        private void googlebetalink_LinkClicked(object sender, HtmlElementEventArgs e)
+        {
+            goToSite("www.alpha.google.com", false);
+        }
+
+        private void Label20_Click(object sender, EventArgs e)
+        {
+            hidePrograms();
+            padamshidden.Dock = DockStyle.Fill;
+            padamshidden.Show();
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            goToSite("www.???.com", false);
+        }
+
+
+        private void ToolStripMenuItem21_Click(object sender, EventArgs e)
+        {
+            ((Form)this.TopLevelControl).Close();
+        }
+
         private void goToSite(string url, bool back)
         {
             switch (url)
@@ -132,7 +181,39 @@ namespace TimeHACK.OS.Win95.Win95Apps
         {
             goToSite(addressbar.Text, false);
         }
-        
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            loadplz.Start();
+        }
+
+        private void loadplz_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (currentsite.Title)
+                {
+                    case "IE4START":
+                        webBrowser1.Document.GetElementById("google").Click += new HtmlElementEventHandler(google_LinkClicked);
+                        webBrowser1.Document.GetElementById("padams").Click += new HtmlElementEventHandler(padams_LinkClicked);
+                        break;
+                    case "12PADAMS":
+                        webBrowser1.Document.GetElementById("wc_b").Click += new HtmlElementEventHandler(WCDownloadButton_Click);
+                        if (!TitleScreen.frm95.hiddenpadamsFound) webBrowser1.Document.GetElementById("distort").Style += "visibility:hidden;";
+                        break;
+                    case "GOOGLE":
+                        webBrowser1.Document.GetElementById("prototype").Click += new HtmlElementEventHandler(googleprototypelink_LinkClicked);
+                        webBrowser1.Document.GetElementById("alpha").Click += new HtmlElementEventHandler(googlebetalink_LinkClicked);
+                        break;
+                }
+                loadplz.Stop();
+            }
+            catch
+            {
+
+            }
+        }
+
         //TODO: Add more websites
         //TODO: Relabel Buttons And Things
     }
